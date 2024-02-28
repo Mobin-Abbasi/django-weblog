6288b24e22df1e04912b8bf346514c0c89b340df
Mobin <mobin04dev@gmail.com>
2024-02-28 23:52:29 +0330

    add portfolio models feature and coding admin

--- weblog/admin.py
+++ weblog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'skills', 'project_completion', 'created', 'updated']
+    list_filter = ['skills', 'project_completion', 'created', 'updated']
+    search_fields = ['title', 'description']
+    prepopulated_fields = {'slug': ['title']}
+    raw_id_fields = ['author']
--- weblog/models.py
+++ weblog/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Portfolio(models.Model):
+    # relation
+    author = models.ForeignKey(User, related_name='user_portfolios', on_delete=models.CASCADE, default='1')
+    # data
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    skills = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
+    # date
+    project_completion = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=(['-created']))
+        ]
+
+    def __str__(self):
+        return self.title
